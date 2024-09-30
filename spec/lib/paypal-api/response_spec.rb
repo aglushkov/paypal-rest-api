1894e6ad512b79f6854d40a7c04f549be4b354a0
Andrey Glushkov <aglushkov@shakuro.com>
2024-09-30 11:11:11 +0300

    Add #follow_up_link method to Response object
    Allows to request HATEOAS link from response.
    
    This is most usefull to navigate through list request pages
    
    ```ruby
    next_page = response.follow_up_link("next") # => [Response] 2nd page response
    next_page.follow_up_link("next") # => [Response] # 3rd page respnse
    next_page.follow_up_link("next") # => nil, no new pages
    ```

--- spec/lib/paypal-api/response_spec.rb
+++ spec/lib/paypal-api/response_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe PaypalAPI::Response do
     instance_double(
       Net::HTTPResponse,
       code: "200",
-      body: body,
+      body: response_body,
       each_header: each_header
     )
   end
 
-  let(:body) { '{"foo":"bar"}' }
+  let(:response_body) { '{"foo":"bar"}' }
   let(:each_header) { {a: 1, b: 2}.each }
-  let(:request) { "REQUEST" }
+  let(:request) { instance_double(PaypalAPI::Request, client: client) }
+  let(:client) { instance_double(PaypalAPI::Client, get: :get_response, post: :post_response) }
 
   before { allow(http_response).to receive(:[]).with("content-type").and_return("application/json") }
 
@@ -24,14 +25,14 @@ RSpec.describe PaypalAPI::Response do
       expect(response.http_headers).to eq(a: 1, b: 2)
       expect(response.http_response).to equal http_response
       expect(response.http_status).to equal 200
-      expect(response.http_body).to equal body
+      expect(response.http_body).to equal response_body
       expect(response.body).to eq(foo: "bar")
       expect(response[:foo]).to eq "bar"
       expect(response.fetch(:foo)).to eq "bar"
     end
 
     context "with non-parsable body" do
-      let(:body) { 123 }
+      let(:response_body) { 123 }
 
       it "returns original http body" do
         expect(response.body).to eq(123)
@@ -47,8 +48,8 @@ RSpec.describe PaypalAPI::Response do
       expect(response.http_headers).to eq(a: 1, b: 2)
       expect(response.http_response).to equal http_response
       expect(response.http_status).to equal 200
-      expect(response.http_body).to equal body
-      expect(response.body).to eq(body)
+      expect(response.http_body).to equal response_body
+      expect(response.body).to eq(response_body)
       expect(response[:foo]).to be_nil
       expect { response.fetch(:foo) }.to raise_error KeyError
     end
@@ -58,6 +59,89 @@ RSpec.describe PaypalAPI::Response do
     it "shows only status" do
       response.body # load body
       expect(response.inspect).to eq "#<PaypalAPI::Response (200)>"
+    end
+  end
+
+  describe "#follow_up_link" do
+    let(:response_body) { JSON.dump(links: links) }
+    let(:query) { "QUERY" }
+    let(:body) { "BODY" }
+    let(:headers) { "HEADERS" }
+
+    context "with GET link" do
+      let(:links) { [{rel: "get_rel", href: "https://api-m.paypal.com/get", method: "GET"}] }
+
+      it "follows up GET link" do
+        expect(response.follow_up_link("get_rel")).to eq :get_response
+
+        expect(client)
+          .to have_received(:get)
+          .with("https://api-m.paypal.com/get", query: nil, body: nil, headers: nil)
+      end
+
+      it "follows up GET link with params" do
+        expect(response.follow_up_link("get_rel", query: query, body: body, headers: headers)).to eq :get_response
+
+        expect(client)
+          .to have_received(:get)
+          .with("https://api-m.paypal.com/get", query: query, body: body, headers: headers)
+      end
+    end
+
+    context "with POST link" do
+      let(:links) { [{rel: "post_rel", href: "https://api-m.paypal.com/post", method: "POST"}] }
+
+      it "follows up POST link" do
+        expect(response.follow_up_link("post_rel")).to eq :post_response
+
+        expect(client)
+          .to have_received(:post)
+          .with("https://api-m.paypal.com/post", query: nil, body: nil, headers: nil)
+      end
+
+      it "follows up POST link with params" do
+        expect(response.follow_up_link("post_rel", query: query, body: body, headers: headers)).to eq :post_response
+
+        expect(client)
+          .to have_received(:post)
+          .with("https://api-m.paypal.com/post", query: query, body: body, headers: headers)
+      end
+    end
+
+    context "with link without method" do
+      let(:links) { [{rel: "get_rel", href: "https://api-m.paypal.com/get"}] }
+
+      it "uses GET method" do
+        expect(response.follow_up_link("get_rel")).to eq :get_response
+
+        expect(client)
+          .to have_received(:get)
+          .with("https://api-m.paypal.com/get", query: nil, body: nil, headers: nil)
+      end
+    end
+
+    context "when response is not a Hash" do
+      let(:response_body) { "foobar" }
+
+      it "returns nil" do
+        expect(response.follow_up_link("get_rel")).to be_nil
+      end
+    end
+
+    context "when response has no links" do
+      let(:response_body) { "{}" }
+
+      it "returns nil" do
+        expect(response.follow_up_link("get_rel")).to be_nil
+      end
+    end
+
+    context "when response has no link with provided rel" do
+      let(:links) { [] }
+
+      it "returns nil" do
+        expect(response.follow_up_link("get_rel")).to be_nil
+      end
     end
   end
 end
